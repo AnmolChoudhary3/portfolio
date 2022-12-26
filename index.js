5b823fac79303021704015fc546c5b995be07e4e
Anmol Choudhary <3anmol3@gmail.com>
2022-12-26 17:25:36 +0530

    minor updates

--- index.js
+++ index.js
@@ -202,3 +202,13 @@ window.onscroll = function() {
 //   }
 //   prevScrollpos = currentScrollPos;
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {scrollFn()};
+
+function scrollFn() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
